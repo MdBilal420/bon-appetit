c190184175a53ee64aa54c9a06bccff8707e4b55
Mohammad Bilal <mohdbilal16@gmail.com>
2020-12-11 14:47:04 +0530

    ratings added

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import './App.css';
 
 const food = {
   starters : [
-    { name : "Fried Ravoli", price : "$5.80"},
-    {name : "Calamari" , price: "$8.98"},
-    {name : "Zucchini Fries" , price: "$9.20"},
+    { name : "🍪 Fried Ravoli", price : "💲 5.80",ratings:"⭐ 3.6/5"},
+    {name : "🍪 Calamari" , price: "💲 8.98",ratings:"⭐ 4.0/5"},
+    {name : "🍪 Zucchini Fries" , price: "💲 9.20",ratings:"⭐ 4.4/5"},
   ],
   sandwiches : [
-    {name : "Grilled Cheese", price : "$8.20"},
-    {name : "Pescaraz Club", price : "$7.50"},
-    {name : "Philly Steak Sub",price : "$4.92"}
+    {name : "🥪 Grilled Cheese", price : "💲 8.20",ratings:"⭐ 3.8/5"},
+    {name : "🥪 Pescaraz Club", price : "💲 7.50",ratings:"⭐ 4.2/5"},
+    {name : "🥪 Philly Steak Sub",price : "💲 4.92",ratings:"⭐ 4.6/5"}
   ],
   pasta : [
-    {name : "Tortelli", price : "$9.20"},
-    {name : "Italian Pasta", price : "$5.80"},
-    {name : "Sphageti Sisco", price : "$6.10"}
+    {name : "🍝 Tortelli", price : "💲 9.20",ratings:"⭐ 4.0/5"},
+    {name : "🍝 Italian Pasta", price : "💲 5.80",ratings:"⭐ 4.4/5"},
+    {name : "🍝 Sphageti Sisco", price : "💲 6.10",ratings:"⭐ 4.8/5"}
   ]
 }
 
 function App() {
-  const headingText = "🍟 bon appétit";
+  const headingText = "🍟 buon appetito";
 
   const [type,setType] = useState("starters");
 
@@ -34,34 +34,22 @@ function App() {
       <header className="App-header">
        <h1>{headingText}</h1>
       </header>
-      <div className="menu">
-        <div className="menu-item">
-          <div className="item">
-          <button onClick={()=>clickHandler("starters")}>🍪 Starters</button>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="item">
-          <button onClick={()=>clickHandler("sandwiches")}>🥪 Sandwiches</button>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="item">
-          <button onClick={()=>clickHandler("pasta")}>🍝 Pasta</button>
-          </div>
-        </div>         
-      </div>
-      <hr /> <hr />
       <div>
+        <button onClick={()=>clickHandler("starters")}>🍪 Starters</button>
+        <button onClick={()=>clickHandler("sandwiches")}>🥪 Sandwiches</button>
+        <button onClick={()=>clickHandler("pasta")}>🍝 Pasta</button>
+      </div>  
+      <hr /> <hr />
+      <div style={{textAlign:"left"}}>
       {
         food[type].map((item) =>{
           return (
-          
-            <ul className="cards">
-              <li key={item.name} className="cards-item">
+            <ul className="lists">
+              <li key={item.name} className="lists-item">
                 <div className="card">
-                  <div className="item-name">{item.name}</div>
-                  <div className="item-price">{item.price}</div>
+                  <p style={{color:"darkred"}}>{item.name}</p>
+                    <p>{item.price}</p>
+                    <p style={{color:"darkgreen",fontWeight:"bold"}}>{item.ratings}</p>
                 </div>
               </li>
             </ul>
