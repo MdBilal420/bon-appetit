1df452aa523c38273db47d07f63f3de3738a92b9
Mohammad Bilal <mohdbilal16@gmail.com>
2020-12-11 11:05:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const food = {
+  starters : [
+    { name : "Fried Ravoli", price : "$5.80"},
+    {name : "Calamari" , price: "$8.98"},
+    {name : "Zucchini Fries" , price: "$9.20"},
+  ],
+  sandwiches : [
+    {name : "Grilled Cheese", price : "$8.20"},
+    {name : "Pescaraz Club", price : "$7.50"},
+    {name : "Philly Steak Sub",price : "$4.92"}
+  ],
+  pasta : [
+    {name : "Tortelli", price : "$9.20"},
+    {name : "Italian Pasta", price : "$5.80"},
+    {name : "Sphageti Sisco", price : "$6.10"}
+  ]
+}
+
 function App() {
+  const headingText = "🍟 bon appétit";
+
+  const [type,setType] = useState("starters");
+
+  const clickHandler = (type) =>{
+    setType(type);
+  }
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>{headingText}</h1>
       </header>
+      <div className="menu">
+        <div className="menu-item">
+          <div className="item">
+          <button onClick={()=>clickHandler("starters")}>🍪 Starters</button>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="item">
+          <button onClick={()=>clickHandler("sandwiches")}>🥪 Sandwiches</button>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="item">
+          <button onClick={()=>clickHandler("pasta")}>🍝 Pasta</button>
+          </div>
+        </div>         
+      </div>
+      <hr /> <hr />
+      <div>
+      {
+        food[type].map((item) =>{
+          return (
+          
+            <ul className="cards">
+              <li key={item.name} className="cards-item">
+                <div className="card">
+                  <div className="item-name">{item.name}</div>
+                  <div className="item-price">{item.price}</div>
+                </div>
+              </li>
+            </ul>
+            )
+          //console.log(item.name)
+        })
+      }
+      </div>      
     </div>
   );
 }
